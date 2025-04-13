99c9a110517d4a430d521f2df4fc1b41db54f30d
sethu786 <75379267+sethu786@users.noreply.github.com>
2025-04-13 14:58:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
 app.use(cors())
 const dbPath = path.join(__dirname, 'candidates.db')
 let db
